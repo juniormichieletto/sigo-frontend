5652cba41024fbd0bbc900cd47c6f917bffad6f9
Junior Michieletto <juniormichieletto@gmail.com>
2021-04-07 00:26:46 +0200

    ADd primeng and start the menu

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+
+@NgModule({
+  declarations: [
+    MenuBarComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule
+  ],
+  exports: [
+    MenuBarComponent
+  ]
+})
+export class CoreModule { }
---
+++ src/app/core/menu-bar/menu-bar.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.css']
+})
+export class MenuBarComponent implements OnInit {
+
+  items: MenuItem[];
+
+    ngOnInit() {
+      this.items = [
+          {
+              label:'Gestao normas',
+              icon:'pi pi-book',
+              items:[
+                  {
+                      label:'Cadastrar/Alterar norma',
+                      icon:'pi pi-fw pi-bookmark',
+                  },
+                  {
+                    separator:true
+                  },
+                  {
+                      label:'Visualizar normas',
+                      icon:'pi pi-bars'
+                  },
+                  {
+                      separator:true
+                  },
+                  {
+                    label:'Histórico de mudanças',
+                    icon:'pi pi-table'
+                },
+              ]
+          },
+          {
+            label:'Consultorias e Assessorias',
+            icon:'pi pi-bookmark',
+            disabled: true
+          },
+          {
+            label:'Gestão do Processo Industrial',
+            icon:'pi pi-chart-bar',
+            disabled: true
+          },
+          {
+              label:'Integracoes',
+              icon:'pi pi-fw pi-wifi',
+              items:[
+                {
+                  label:'Legados',
+                  icon:'pi pi-fw pi-tablet',
+                  disabled: true
+                },
+                  {
+                      label:'Webhook`s',
+                      icon:'pi pi-fw pi-wifi',
+                      disabled: true
+                  },
+             ]
+          },
+          {
+              label:'Desconectar',
+              icon:'pi pi-fw pi-power-off'
+          }
+      ];
+    }
+}
